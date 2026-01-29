d99487e142e419a89ff435ece66798a01636d0fd
GUIFRE88 <guilherme.fre073@hotmail.com>
2026-01-29 08:33:07 -0300

    Remove production database configuration and clean up development host settings
    - Eliminated explicit production database configuration from database.yml to enhance security and maintainability.
    - Removed the Railway host from the development environment configuration, streamlining the code and ensuring proper closure of the configuration block.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,4 @@ Rails.application.configure do
 
   config.assets.quiet = true
 
-  config.hosts << "scrap-production-8543.up.railway.app"
 end
-
-Rails.application.config.hosts << "scrap-production-8543.up.railway.app"
