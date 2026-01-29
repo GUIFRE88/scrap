3e7f25e01c22ee556e20d4e73a1514acba5598b6
GUIFRE88 <guilherme.fre073@hotmail.com>
2026-01-29 06:54:25 -0300

    Enhance production environment configuration to support SECRET_KEY_BASE
    - Added support for setting SECRET_KEY_BASE via an environment variable, improving security and flexibility in the production environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,9 @@ Rails.application.configure do
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
+  
+  # Allow SECRET_KEY_BASE to be set via environment variable (Railway)
+  config.secret_key_base = ENV["SECRET_KEY_BASE"] if ENV["SECRET_KEY_BASE"].present?
 
   # Enable serving static files for Railway
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || true
